aef6afc12f05407c14958d5ea41ca33c3da7e5b5
arigelis <workstance@mail.ru>
2019-03-29 23:21:30 +0500

    1.4. JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+/**
+*CalculateTest  class
+*/
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Test.
+ *
+ * @author Fedor Teslyuk (workstance@mail.ru)
+ * @version $1$
+ * @since 29.03.2019
+ */
+public class CalculateTest{
+/**
+/*Test echo!
+*/
+	@Test
+	 public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "fedor";
+		String expect = "Echo, echo, echo : fedor"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	 }
+}
