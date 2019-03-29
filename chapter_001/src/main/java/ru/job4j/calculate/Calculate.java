92aa55748c1df63a47fc4040d57c1d196c4bcac4
arigelis <workstance@mail.ru>
2019-03-29 20:45:33 +0500

    1.2. Подключить сборщик Maven[#125793]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
+/**
+*Calculate class
+*/
 public class Calculate{
+/**
+*Main
+*@param args - args
+*/
 	public static void main(String args[]){
 			System.out.println("Hello, world!");
 	}
