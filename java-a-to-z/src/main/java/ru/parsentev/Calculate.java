88958526be73510a5f93d4d57cf335ea7fce00c4
arigelis <workstance@mail.ru>
2019-03-29 22:50:38 +0500

    files from video

---
+++ java-a-to-z/src/main/java/ru/parsentev/Calculate.java
@@ -0,0 +1,12 @@
+package main.java.ru.parsentev;
+
+public class Calculate{
+	public String echo(String txt){
+		return(String.format("%S %S %S",txt,txt,txt));
+	}
+	
+	public static void main(String[] args) {
+		Calculate calc = new Calculate();
+		System.out.println(calc.echo("go! "));
+	}
+}
