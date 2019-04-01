37809a4fd4b8b4890bc60095b49aeb4afa8a321e
arigelis <workstance@mail.ru>
2019-04-01 22:28:40 +0500

    4.1. Глупый бот.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+/**
+ * @author Fedor Teslyuk(workstance@mail.ru)
+ * @version $Id$
+ * @since 01.04.2019
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
