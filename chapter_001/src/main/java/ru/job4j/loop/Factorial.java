4aaf0d9fbe4bc0880da624554badde01512814e5
arigelis <workstance@mail.ru>
2019-04-04 00:17:20 +0500

    checkStyle corrects

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,18 +1,24 @@
+/**
+ * Javadoc package
+ */
 package ru.job4j.loop;
 
 /**
- * class for factorial calc
+ * class for factorial calc.
+ *
+ * @since 03.04.2019.
  */
 public class Factorial {
     /**
-     *
-     * @param n
-     * @return
+     * Method calc.
+     * @param n input data for calculating
+     * @return result
+     * @since 03.04.2019.
      */
-    public int calc(int n) {
+    public int calc(final int n) {
         int result = 1;
         for (int i = 1; i <= n; i++) {
-            result*=i;
+            result *= i;
         }
         return result;
     }
---
+++ chapter_001/src/test/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ * test class
+ * @author Fedor Teslyuk (workstance@mail.ru)
+ * @version 1
+ * @since 28.03.2019
+ */
