68f0c7837868e513e80ab55d9446b5e1cc53f182
arigelis <workstance@mail.ru>
2019-04-05 00:24:29 +0500

    Algorithmic upgrade.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,20 +1,14 @@
 package ru.job4j.array;
 
-//4,1,6,2
+/**
+ * cooool
+ */
 public class Turn {
     public int[] back(int[] array) {
-        if (array.length % 2 == 0) {
-            for (int i = 0; i < array.length/2; i++) {
-                int buf = array[i];
-                array[i] = array[array.length - 1 - i];
-                array[array.length - 1 - i] = buf;
-            }
-        }else{
-            for (int i = 0; i < array.length/2+1; i++) {
-                int buf = array[i];
-                array[i] = array[array.length - 1 - i];
-                array[array.length - 1 - i] = buf;
-            }
+        for (int i = 0; i < array.length / 2; i++) {
+            int buf = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = buf;
         }
         return array;
     }
