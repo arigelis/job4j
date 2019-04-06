8cf166a9fa7bf8ee2e40d676d64882005cbdf8c6
arigelis <workstance@mail.ru>
2019-04-06 19:42:43 +0500

    duplicateRemove edition

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -4,21 +4,31 @@ import java.util.Arrays;
 
 public class ArrayDuplicate {
     public String[] remove(String[] array) {
-        int duplicatesFound = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length - duplicatesFound; j++) {
-                if (array[i].equals(array[j])) {
-                    while (j != array.length - duplicatesFound - 1 && array[array.length - duplicatesFound - 1].equals(array[j])) {
-                        duplicatesFound++;
-                    }
-
-                    String buf = array[array.length - duplicatesFound - 1];
-                    array[array.length - duplicatesFound - 1] = array[j];
-                    array[j] = buf;
-                    duplicatesFound++;
+//        int duplicatesFound = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = i + 1; j < array.length - duplicatesFound; j++) {
+//                if (array[i].equals(array[j])) {
+//                    while (j != array.length - duplicatesFound - 1 && array[array.length - duplicatesFound - 1].equals(array[j])) {
+//                        duplicatesFound++;
+//                    }
+//
+//                    String buf = array[array.length - duplicatesFound - 1];
+//                    array[array.length - duplicatesFound - 1] = array[j];
+//                    array[j] = buf;
+//                    duplicatesFound++;
+//                }
+//            }
+//        }
+        int unique = array.length;
+        for (int out = 0; out < unique; out++) {
+            for (int in = out + 1; in < unique; in++) {
+                if (array[out].equals(array[in])) {
+                    array[in] = array[unique - 1];
+                    unique--;
+                    in--;
                 }
             }
         }
-        return Arrays.copyOf(array, array.length - duplicatesFound);
+        return Arrays.copyOf(array, unique);
     }
 }
