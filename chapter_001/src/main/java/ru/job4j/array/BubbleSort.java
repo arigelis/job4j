366ae144e67b2bc2520fa989d7e1e2ce618928b1
arigelis <workstance@mail.ru>
2019-04-06 23:10:59 +0500

    bubble trouble

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -2,16 +2,19 @@ package ru.job4j.array;
 
 public class BubbleSort {
     public int[] sort(int[] array) {
+        int iter = 0;
         for (int j = 0; j < array.length; j++) {
-            for (int i = 1; i < array.length; i++) {
+            for (int i = 1; i < array.length-j; i++) {
                 if (array[i - 1] > array[i]) {
                     int buf = array[i];
                     array[i] = array[i - 1];
                     array[i - 1] = buf;
                     i = j;
+                    iter ++;
                 }
             }
         }
+        System.out.println(iter);
         return array;
     }
 }
