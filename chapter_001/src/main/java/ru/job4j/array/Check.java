4b0f53e753b5471c91b12f97813d26b636a89c9e
arigelis <workstance@mail.ru>
2019-04-06 20:20:34 +0500

    checkStyle for check class

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -1,12 +1,21 @@
 package ru.job4j.array;
 
+/**
+ *class for checking.
+ *since 06.04.2019
+ */
 public class Check {
-    public boolean mono(boolean[] data) {
-        boolean result = false;
+    /**
+     * @param data boolean
+     * @return boolean
+     */
+    public boolean mono(final boolean[] data) {
+        boolean result;
         result = data[0];
         for (int i = 1; i < data.length; i++) {
-            if(data[i]!=data[i-1]){
+            if (data[i] != data[i - 1]) {
                 result = false;
+                break;
             }
         }
         return result;
---
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ * test class
+ * @author Fedor Teslyuk (workstance@mail.ru)
+ * @version 1
+ * @since 28.03.2019
+ */
+package ru.job4j.array;
