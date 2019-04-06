635d36fdb311dba2f56727b164efd7c2ed7aba1b
arigelis <workstance@mail.ru>
2019-04-06 20:23:42 +0500

    Matrix checkstyle

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -1,12 +1,22 @@
 package ru.job4j.array;
 
+/**
+ * class of matrix.
+ *
+ * @author me
+ * @since 06.04.2019
+ */
 public class Matrix {
-    public int[][] multiple(int size){
+    /**
+     * @param size int
+     * @return int[][]
+     */
+    public int[][] multiple(final int size) {
         int[][] matrix = new int[size][size];
         matrix[0][0] = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                matrix[i][j] = (i+1) * (j+1);
+                matrix[i][j] = (i + 1) * (j + 1);
             }
         }
         return matrix;
