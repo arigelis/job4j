1632a0b9fb61dead7ddb11a7d56b4b35aae333b9
arigelis <workstance@mail.ru>
2019-04-06 23:44:39 +0500

    MatrixCheck final

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,13 +1,23 @@
 package ru.job4j.array;
 
+/**
+ * matrix check class.
+ *
+ * @author me
+ * @since 06.04.2019
+ */
 public class MatrixCheck {
-    public boolean mono(boolean[][] data) {
+    /**
+     * @param data for boolean array2x
+     * @return boolean
+     */
+    public boolean mono(final boolean[][] data) {
         boolean result = true;
         boolean tmp = data[0][0];
-        boolean tmp2 = data[0][data[0].length-1];
+        boolean tmp2 = data[0][data.length - 1];
         for (int i = 1; i < data.length; i++) {
-            if (data[i][i] != tmp && data[i][data[i].length-1]!=tmp2) {
-                return false;
+            if (data[i][i] != tmp || data[i][data.length - i - 1] != tmp2) {
+                result = false;
             }
         }
         return result;
