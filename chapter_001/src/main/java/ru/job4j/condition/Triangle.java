d1ace857aa65e5445063e3308e70b4dc7c50842f
arigelis <workstance@mail.ru>
2019-04-10 22:36:48 +0500

    2. Рефакторинг - Площадь треугольника.

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,6 +2,15 @@ package ru.job4j.condition;
 
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
 
     /**
      * Метод вычисления периметра по длинам сторон.
@@ -20,7 +29,7 @@ public class Triangle {
     }
 
     /**
-     * Метод должен вычислить прощадь треугольканива.
+     * Метод должен вычислить площадь треугольника.
      * <p>
      * Формула.
      * <p>
@@ -28,13 +37,13 @@ public class Triangle {
      * <p>
      * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
      *
-     * @return Вернуть прощадь, если треугольник существует или -1.
+     * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rsl = -1;
-        double a = new Point(x1, y1).distance(new Point(x2, y2));
-        double b = new Point(x2, y2).distance(new Point(x3, y3));
-        double c = new Point(x1, y1).distance(new Point(x3, y3));
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
         double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
