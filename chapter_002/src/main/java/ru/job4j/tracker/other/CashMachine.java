4cd8001a0d35f378762252a6022f5203f7958e75
arigelis <workstance@mail.ru>
2019-05-07 21:14:24 +0500

    CashMachine refactor

--- chapter_002/src/main/java/ru/job4j/tracker/other/CashMachine.java
+++ chapter_002/src/main/java/ru/job4j/tracker/other/CashMachine.java
@@ -1,6 +1,3 @@
-
-
-
 package ru.job4j.tracker.other;
 
 import java.util.ArrayList;
@@ -50,64 +47,3 @@ public class CashMachine {
         return answers;
     }
 }
-
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Collections;
-//import java.util.List;
-//
-//public class CashMachine {
-//
-//    public CashMachine(final int[] values) {
-//        Arrays.sort(new int[][]{values}, Collections.reverseOrder().reversed());
-//        this.values = values;
-//    }
-//
-//    private final int[] values;
-//
-//    public List<List<Integer>> exchange(int note) {
-//        List<List<Integer>> answers = new ArrayList<>();
-//        int counter = 0;
-//        int noteTmp1 = note;
-//        answers.add(new ArrayList<>());
-//        for (int i = 0; i < values.length; i++) {
-//            while (noteTmp1 / values[i] > 0) {
-//                answers.get(0).add(values[i]);
-//                noteTmp1 -= values[i];
-//            }
-//        }
-//        noteTmp1 = note;
-//        counter++;
-//        answers.add(new ArrayList<>());
-//        for (int i = 1; i < values.length; i++) {
-//            while (noteTmp1 / values[i] > 0) {
-//                answers.get(1).add(values[i]);
-//                noteTmp1 -= values[i];
-//            }
-//        }
-//        noteTmp1 = note;
-//        counter++;
-//        answers.add(new ArrayList<>());
-//        for (int i = 2; i < values.length; i++) {
-//            while (noteTmp1 / values[i] > 0) {
-//                answers.get(2).add(values[i]);
-//                noteTmp1 -= values[i];
-//            }
-//        }
-//        return answers;
-//    }
-//}
-
-
-
-class calculator{
-    int a,b;
-    int result;
-
-    //function
-    public void add(int a,int b){
-        result = a+b;
-    }
-}
-
