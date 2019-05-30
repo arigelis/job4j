102ad0cfa08164ad50a78e6e3fb74d7031f2692e
arigelis <workstance@mail.ru>
2019-05-30 22:03:13 +0500

    1. Collection API Улучшения V2

--- chapter_003/src/test/java/ru/job4j/search/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/search/UserTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -18,6 +19,8 @@ public class UserTest {
         User user2 = new User(2, "Christine", "Denver");
         HashMap<Integer, User> usersMap = userConvert.process(new ArrayList<>(Arrays.asList(user1, user2)));
         HashMap<Integer, User> expect = new HashMap<>();
+
+//        HashMap<Integer, User> expect3 = Map.of(1, user1, 2, user2);
         expect.put(1, user1);
         expect.put(2, user2);
         assertThat(usersMap, is(expect));
